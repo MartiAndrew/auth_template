13c8d6762846998e7243c96418a33bb96ea8674d
Andrew <andrew-bee@mail.ru>
2025-11-10 10:48:48 +0300

    create id primary key mixin

---
+++ tmpauth/db/models/mixins/id_int_pk.py
@@ -0,0 +1,6 @@
+from sqlalchemy.orm import Mapped, mapped_column
+
+class IdIntPkMixin:
+    """Миксин для моделей с целочисленным первичным ключом."""
+
+    id: Mapped[int] = mapped_column(primary_key=True)
--- tmpauth/db/models/user.py
+++ tmpauth/db/models/user.py
@@ -1,8 +1,35 @@
+from datetime import UTC, datetime
+from typing import TYPE_CHECKING
+
+from fastapi_users_db_sqlalchemy import (
+    SQLAlchemyBaseUserTable,
+    SQLAlchemyUserDatabase as SQLAlchemyUserDatabaseGeneric,
+)
+from sqlalchemy import DateTime, String, func
+from sqlalchemy.orm import relationship, Mapped
+
+from core.types.user_id import UserIdType
+
+
 from sqlalchemy.orm import Mapped, mapped_column
 from tmpauth.db.models import Base
+from tmpauth.db.models.mixins.id_int_pk import IdIntPkMixin
 
 
-class User(Base):
+class User(Base, IdIntPkMixin, SQLAlchemyBaseUserTable[int]):
     """Модель пользователя."""
 
-    user: Mapped[str] = mapped_column(unique=True)
+    nickname: Mapped[str | None] = mapped_column(
+        String(length=25), unique=True, comment="Никнейм", nullable=True
+    )
+    avatar: Mapped[str | None] = mapped_column(
+        String(length=250), comment="Ссылка на аватар", nullable=True
+    )
+    about: Mapped[str | None] = mapped_column(
+        String(length=150), comment="О себе", nullable=True
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        comment="Дата создания",
+        server_default=func.now(),
+    )
