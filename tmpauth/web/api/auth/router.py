a09713cae822300195da5d69e195ce50199d2779
Andrew <andrew-bee@mail.ru>
2025-11-10 18:22:49 +0300

    add register route

--- tmpauth/web/api/auth/router.py
+++ tmpauth/web/api/auth/router.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter
+
+from tmpauth.db.schemas.user import UserCreate, UserRead
 from tmpauth.services.authentication.auth_backend import authentication_backend
 from tmpauth.services.authentication.fastapi_users import fastapi_users
 
@@ -6,3 +8,5 @@ auth_router = APIRouter()
 
 
 auth_router.include_router(router=fastapi_users.get_auth_router(authentication_backend))
+
+auth_router.include_router(router=fastapi_users.get_register_router(UserRead, UserCreate))
