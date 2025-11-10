23bc624fc5b5ac9ca057c1f615e9a9037d85712b
Andrew <andrew-bee@mail.ru>
2025-11-10 14:47:01 +0300

    create  user_manager dependence

--- tmpauth/services/authentication/dependencies.py
+++ tmpauth/services/authentication/dependencies.py
@@ -1,9 +1,12 @@
 from typing import TYPE_CHECKING, Annotated
 
-from fastapi import Depends
+from fastapi import BackgroundTasks, Depends
+from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
+
 from tmpauth.db.models import AccessToken, User
 
 from common.sqlalchemy.dependencies import get_db_session
+from tmpauth.services.authentication.user_manager import UserManager
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
@@ -25,3 +28,17 @@ async def get_users_db(
     ],
 ):
     yield User.get_db_user(session=session)
+
+
+
+async def get_user_manager(
+    users_db: Annotated[
+        "SQLAlchemyUserDatabase",
+        Depends(get_users_db),
+    ],
+    background_tasks: BackgroundTasks,
+):
+    yield UserManager(
+        users_db,
+        background_tasks=background_tasks,
+    )
