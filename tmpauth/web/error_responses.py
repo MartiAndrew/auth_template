f961306e44e59bc54c425ce0df350789e5b577dc
Andrew <andrew-bee@mail.ru>
2025-11-12 20:58:13 +0300

    refactoring

--- tmpauth/web/error_responses.py
+++ tmpauth/web/error_responses.py
@@ -4,23 +4,3 @@ from common.errors.schema import ErrorResponse, ErrResponseBody
 
 PARAMS_VALIDATION_ERR_CODE = 6
 PARAMS_MISSING_ERR_CODE = 7
-PARAMS_NOT_FOUND_ERR_CODE = 4
-PARAMS_CHECK_VIOLATION_ERR_CODE = 5
-
-order_not_found = ErrorResponse(
-    body=ErrResponseBody(
-        message="Заказ не найден.",
-        error_code=PARAMS_NOT_FOUND_ERR_CODE,
-        verbose_message="Заказ не найден.",
-    ),
-    status_code=status.HTTP_404_NOT_FOUND,
-)
-
-order_check_violation = ErrorResponse(
-    body=ErrResponseBody(
-        message="Недостаточное количество товара.",
-        error_code=PARAMS_CHECK_VIOLATION_ERR_CODE,
-        verbose_message="Недостаточное количество товара.",
-    ),
-    status_code=status.HTTP_409_CONFLICT,
-)
--- tmpauth/web/exceptions.py
+++ tmpauth/web/exceptions.py
@@ -1,24 +1,8 @@
 from starlette import status
-from tmpauth.web.error_responses import (  # noqa: WPS235
-    PARAMS_VALIDATION_ERR_CODE,
-    order_check_violation,
-    order_not_found,
-)
+from tmpauth.web.error_responses import PARAMS_VALIDATION_ERR_CODE  # noqa: WPS235
 
 from common.errors.exceptions import ServiceError
 from common.errors.schema import ErrorResponse, ErrResponseBody
-
-
-class OrderNotFoundError(ServiceError):
-    """Заказ не найден."""
-
-    response_data: ErrorResponse = order_not_found
-
-
-class OrderCheckViolationError(ServiceError):
-    """Ошибка консистентности продукта в заказе."""
-
-    response_data: ErrorResponse = order_check_violation
 
 
 class ParameterValidationError(ServiceError):
